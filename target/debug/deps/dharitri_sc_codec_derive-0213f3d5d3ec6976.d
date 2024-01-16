a07d9d9768d4f754f111d384a956dfed602f6291
Saisumanthv <104812692+Saisumanthv@users.noreply.github.com>
2024-01-16 18:30:02 +0530

    0.3
    done some  'find and replace' in code.

--- target/debug/deps/dharitri_sc_codec_derive-0213f3d5d3ec6976.d
+++ target/debug/deps/dharitri_sc_codec_derive-0213f3d5d3ec6976.d
@@ -1,6 +1,6 @@
-/home/sumanth/Desktop/project/mx-sdk-rs (renamed) (vs code renamed)/target/debug/deps/libmultiversx_sc_codec_derive-0213f3d5d3ec6976.so: data/codec-derive/src/lib.rs data/codec-derive/src/nested_de_derive.rs data/codec-derive/src/nested_en_derive.rs data/codec-derive/src/top_de_derive.rs data/codec-derive/src/top_en_derive.rs data/codec-derive/src/util.rs
+/home/sumanth/Desktop/project/mx-sdk-rs (renamed) (vs code renamed)/target/debug/deps/libdharitri_sc_codec_derive-0213f3d5d3ec6976.so: data/codec-derive/src/lib.rs data/codec-derive/src/nested_de_derive.rs data/codec-derive/src/nested_en_derive.rs data/codec-derive/src/top_de_derive.rs data/codec-derive/src/top_en_derive.rs data/codec-derive/src/util.rs
 
-/home/sumanth/Desktop/project/mx-sdk-rs (renamed) (vs code renamed)/target/debug/deps/multiversx_sc_codec_derive-0213f3d5d3ec6976.d: data/codec-derive/src/lib.rs data/codec-derive/src/nested_de_derive.rs data/codec-derive/src/nested_en_derive.rs data/codec-derive/src/top_de_derive.rs data/codec-derive/src/top_en_derive.rs data/codec-derive/src/util.rs
+/home/sumanth/Desktop/project/mx-sdk-rs (renamed) (vs code renamed)/target/debug/deps/dharitri_sc_codec_derive-0213f3d5d3ec6976.d: data/codec-derive/src/lib.rs data/codec-derive/src/nested_de_derive.rs data/codec-derive/src/nested_en_derive.rs data/codec-derive/src/top_de_derive.rs data/codec-derive/src/top_en_derive.rs data/codec-derive/src/util.rs
 
 data/codec-derive/src/lib.rs:
 data/codec-derive/src/nested_de_derive.rs:
